fae4ae03a698cf4d9e0fb3888fefd7a4095f5403
skilllinkacademy-blip <skilllink.academy@gmail.com>
2026-03-01 22:01:02 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>SkillLink | חיבור בין מקצוענים</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </Head>
+      <div dir="rtl">
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
